0f27eb2cc94be649308748da5e06a0ec1bc23588
divfadli <fadlilahdivyy@gmail.com>
2024-11-17 13:25:53 +0100

    Add resource pengguna

---
+++ app/http/controllers/pengguna_controller.go
@@ -0,0 +1,56 @@
+package controllers
+
+import (
+	"goravel/app/models"
+
+	"github.com/goravel/framework/contracts/http"
+	"github.com/goravel/framework/facades"
+	"github.com/goravel/framework/support"
+)
+
+type PenggunaController struct {
+	//Dependent services
+}
+
+func NewPenggunaController() *PenggunaController {
+	return &PenggunaController{
+		//Inject services
+	}
+}
+
+func (r *PenggunaController) Index(ctx http.Context) http.Response {
+	userInfo := facades.Cache().Get("user_data")
+
+	var dataKaryawan []models.Karyawan
+	facades.Orm().Query().With("Jabatan").With("User.Role").Get(&dataKaryawan)
+
+	if userInfo != nil {
+		return ctx.Response().View().Make("pengguna.tmpl", map[string]interface{}{
+			"title":       "Pengguna",
+			"pageheading": "Pengguna",
+			"version":     support.Version,
+			"data":        userInfo,
+			"pengguna":    dataKaryawan,
+		})
+	}
+
+	facades.Auth().Logout(ctx)
+	// For instance, you might redirect the user to the login page
+	return ctx.Response().Redirect(http.StatusFound, "/login")
+}
+
+func (r *PenggunaController) Show(ctx http.Context) http.Response {
+	return nil
+}
+
+func (r *PenggunaController) Store(ctx http.Context) http.Response {
+	return nil
+}
+
+func (r *PenggunaController) Update(ctx http.Context) http.Response {
+	return nil
+}
+
+func (r *PenggunaController) Destroy(ctx http.Context) http.Response {
+	return nil
+}
