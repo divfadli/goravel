96d7d82182e4d134778a60f2c2b0021c387525b2
divfadli <fadlilahdivyy@gmail.com>
2024-11-17 18:02:08 +0100

    Create and update user

--- app/http/controllers/pengguna_controller.go
+++ app/http/controllers/pengguna_controller.go
@@ -4,6 +4,7 @@ import (
 	"goravel/app/models"
 	"strconv"
 
+	"github.com/golang-module/carbon/v2"
 	"github.com/goravel/framework/contracts/http"
 	"github.com/goravel/framework/facades"
 	"github.com/goravel/framework/support"
@@ -45,12 +46,33 @@ func (r *PenggunaController) Show(ctx http.Context) http.Response {
 
 	if userInfo != nil {
 		id := ctx.Request().Route("id")
+		idParsed := 0
+
 		var dataKaryawan models.Karyawan
-		facades.Orm().Query().With("Jabatan").With("User.Role").Where("user_id=?", id).Get(&dataKaryawan)
+
+		if parseInt, err := strconv.Atoi(id); err == nil {
+			idParsed = parseInt
+		}
+
+		if id != "new" && idParsed > 0 {
+			facades.Orm().Query().With("Jabatan").With("User.Role").Where("user_id=?", id).Get(&dataKaryawan)
+		}
+
+		idAtasan := ""
+
+		if dataKaryawan.IDAtasan != nil {
+			idAtasan = *dataKaryawan.IDAtasan
+		}
+
 		var dataJabatan []models.Jabatan
 		facades.Orm().Query().Get(&dataJabatan)
+
 		var dataRole []models.Role
 		facades.Orm().Query().Get(&dataRole)
+
+		var dataAtasan []models.Karyawan
+		facades.Orm().Query().With("Jabatan").With("User.Role").Get(&dataAtasan)
+
 		return ctx.Response().View().Make("pengguna_detail.tmpl", map[string]interface{}{
 			"title":       "Pengguna",
 			"pageheading": "Pengguna",
@@ -59,6 +81,9 @@ func (r *PenggunaController) Show(ctx http.Context) http.Response {
 			"pengguna":    dataKaryawan,
 			"jabatan":     dataJabatan,
 			"role":        dataRole,
+			"isCreate":    id == "new",
+			"listAtasan":  dataAtasan,
+			"id_atasan":   idAtasan,
 		})
 	}
 
@@ -68,7 +93,45 @@ func (r *PenggunaController) Show(ctx http.Context) http.Response {
 }
 
 func (r *PenggunaController) Store(ctx http.Context) http.Response {
-	return nil
+	nama := ctx.Request().Input("nama")
+	email := ctx.Request().Input("email")
+	nik := ctx.Request().Input("nik")
+	jabatan_id, _ := strconv.Atoi(ctx.Request().Input("jabatan"))
+	role_id, _ := strconv.Atoi(ctx.Request().Input("role"))
+	gender := ctx.Request().Input("gender")
+	agama := ctx.Request().Input("agama")
+	tanggal_lahir := ctx.Request().Input("tanggal_lahir")
+	id_atasan := ctx.Request().Input("atasan")
+
+	var karyawan models.Karyawan
+	karyawan.Name = nama
+	karyawan.EmpNo = nik
+	karyawan.Agama = agama
+	karyawan.Gender = gender
+	karyawan.TanggalLahir = carbon.Parse(tanggal_lahir).ToDateStruct()
+	if id_atasan != "" {
+		karyawan.IDAtasan = &id_atasan
+	} else {
+		karyawan.IDAtasan = nil
+	}
+	karyawan.JabatanID = jabatan_id
+
+	var user models.Akun
+	user.Email = email
+	// TODO: password default
+	user.Password, _ = facades.Hash().Make("12345678")
+	user.RoleID = role_id
+	facades.Orm().Query().Save(&user)
+
+	karyawan.UserID = int(user.IDUser)
+	karyawan.User = user
+	facades.Orm().Query().Save(&karyawan)
+
+	return ctx.Response().Json(200, map[string]interface{}{
+		"status":  "success",
+		"message": "Data berhasil disimpan",
+		"data":    karyawan,
+	})
 }
 
 func (r *PenggunaController) Update(ctx http.Context) http.Response {
@@ -78,6 +141,10 @@ func (r *PenggunaController) Update(ctx http.Context) http.Response {
 	nik := ctx.Request().Input("nik")
 	jabatan_id, _ := strconv.Atoi(ctx.Request().Input("jabatan"))
 	role_id, _ := strconv.Atoi(ctx.Request().Input("role"))
+	gender := ctx.Request().Input("gender")
+	agama := ctx.Request().Input("agama")
+	tanggal_lahir := ctx.Request().Input("tanggal_lahir")
+	id_atasan := ctx.Request().Input("atasan")
 
 	var karyawan models.Karyawan
 	facades.Orm().Query().With("Jabatan").With("User.Role").Where("user_id=?", id).First(&karyawan)
@@ -87,6 +154,14 @@ func (r *PenggunaController) Update(ctx http.Context) http.Response {
 
 	karyawan.Name = nama
 	karyawan.EmpNo = nik
+	karyawan.Agama = agama
+	karyawan.Gender = gender
+	karyawan.TanggalLahir = carbon.Parse(tanggal_lahir).ToDateStruct()
+	if id_atasan != "" {
+		karyawan.IDAtasan = &id_atasan
+	} else {
+		karyawan.IDAtasan = nil
+	}
 	karyawan.JabatanID = jabatan_id
 	karyawan.User.Email = email
 	karyawan.User.RoleID = role_id
